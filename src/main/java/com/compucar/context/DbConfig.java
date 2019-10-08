a227f036ee25fa0071fc6d24d664dd8183070a6a
Marcos Irisarri <marcos@iovlabs.org>
2019-10-08 18:07:33 -0300

    Add reader and workshop classes

--- src/main/java/com/compucar/context/DbConfig.java
+++ src/main/java/com/compucar/context/DbConfig.java
@@ -106,5 +106,4 @@ public class DbConfig {
     }
 
     // JpaRepositories FACTORY END
-
 }
--- src/main/java/com/compucar/model/Workshop.java
+++ src/main/java/com/compucar/model/Workshop.java
@@ -8,10 +8,9 @@ import javax.persistence.Entity;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
-public class Workshop extends  MaintenanceItem {
+public class Workshop extends MaintenanceItem {
     private String code;
     private String name;
     private String address;
     private String city;
-
 }
