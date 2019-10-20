5a62435b215a80e471f0162d09bb61ece1ab932c
Brahian <bcalo93@gmail.com>
2019-10-20 12:28:25 -0300

    Completed traceability register.

---
+++ src/main/java/com/compucar/model/Traceability.java
@@ -0,0 +1,16 @@
+package com.compucar.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+public class Traceability extends DataEntity {
+    private String username;
+    private String serviceName;
+    private LocalDateTime registerDate;
+}
